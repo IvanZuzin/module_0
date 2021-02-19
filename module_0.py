2a44c14a6360bd76439868552e3a216b45955d89
IvanZuzin <Zuzin.iv@gmail.com>
2021-02-19 18:31:38 +0300

    module_0: Corrected notes on the written code

--- module_0.py
+++ module_0.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-number = np.random.randint(1,101)      # загадали число
+number = np.random.randint(1, 101)      # загадали число
 
 print ("Загадано число от 1 до 100")
 
@@ -8,20 +8,31 @@ def game_core_v3(number):
     '''Начинаем с середины диапазона (50), в зависимости от результата 
     больше/меньше определяем новый диапозон в котором продолжаем искать число.
     Функция принимает загаданное число и возвращает число попыток вместе с загаданным числом'''
-    
-    count = 0
+
+    count = 1
     min_range = 1
     max_range = 100
-    predict = 50 # начальная позиция поиска
-    
+    predict = 50  # начальная позиция поиска
+
     while number != predict:
         count += 1
-        if number > predict: 
-            min_range = predict # определение ноговго минимума диапозона поиска
-            predict += int((max_range - min_range)//2 + (1 if (max_range - min_range)//2 == 0 else 0))   
-        elif number < predict: 
-            max_range = predict # определение ноговго максимума диапозона поиска
+        if number > predict:
+            min_range = predict  # определение ноговго минимума диапозона поиска
+            predict += int((max_range - min_range)//2 + (1 if (max_range - min_range)//2 == 0 else 0))
+        elif number < predict:
+            max_range = predict  # определение ноговго максимума диапозона поиска
             predict -= int((max_range - min_range)//2 + (1 if (max_range - min_range)//2 == 0 else 0))
-    return print (f"Вы угадали число {number} за {count} попыток.")
+    return count
 
-game_core_v3(number)
+def score_game(game_core):
+    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+    count_ls = []
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1, 101, size=(1000))
+    for number in random_array:
+        count_ls.append(game_core(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return(score)
+
+score_game(game_core_v3)
